0f70f58ed9b141284c89f6b8a1b893f6849ae526
xubin <xpin_1509@163.com>
2020-05-26 19:32:06 +0800

    提交链表

--- js/leetCode.js
+++ js/leetCode.js
@@ -892,7 +892,15 @@ var climbStairs = function(n) {
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-
+    const cur = head
+    while (cur && cur.next) {
+        if (cur.next.val === cur.val) {
+            cur.next = cur.next.next
+        } else {
+            cur = cur.next
+        }
+    }
+    return head
 };
 // 88. 合并两个有序数组
 // 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
