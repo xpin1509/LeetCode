32f035d2492a259273e0fba6ebdcd46ce00bf422
xubin <xpin_1509@163.com>
2020-05-27 23:05:51 +0800

    罗马数字提交

--- js/top50.js
+++ js/top50.js
@@ -69,3 +69,141 @@ var threeSum = function(nums) {
     }
     return res
 };
+
+// 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
+
+// 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+
+// 你可以假设 nums1 和 nums2 不会同时为空。
+
+// nums1 = [1, 3]
+// nums2 = [2]
+
+// 则中位数是 2.0
+
+// nums1 = [1, 2]
+// nums2 = [3, 4]
+
+// 则中位数是 (2 + 3)/2 = 2.5
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    const res = [...nums1, ...nums2].sort((a, b) => a - b)
+    if (res.length % 2 == 0) {
+        return (res[res.length / 2 - 1] + res[res.length / 2]) / 2
+    } else if (res.length % 2 == 1) {
+        return res[(res.length - 1) / 2]
+    }
+};
+
+// 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+// 输入：[1,8,6,2,5,4,8,3,7]
+// 输出：49
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    const len = height.length
+    let max = 0
+    for (let i = 0; i < len; i++) {
+        for (let j = i + 1; j < len; j ++) {
+            const res = Math.min(height[i], height[j]) * (j - i)
+            max = Math.max(res, max)
+        }
+    }
+    return max
+};
+
+// 罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+// 字符          数值
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+// 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+// I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+// X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+// C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+// 给定一个整数，将其转为罗马数字。输入确保在 1 到 3999 的范围内。
+
+// 输入: 3
+// 输出: "III"
+// 示例 2:
+
+// 输入: 4
+// 输出: "IV"
+// 示例 3:
+
+// 输入: 9
+// 输出: "IX"
+// 示例 4:
+
+// 输入: 58
+// 输出: "LVIII"
+// 解释: L = 50, V = 5, III = 3.
+// 示例 5:
+
+// 输入: 1994
+// 输出: "MCMXCIV"
+// 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    const arr = `${num}`.split('').reverse()
+    const res = []
+    const nomallMap = {
+        1: 'I',
+        5: 'V',
+        10: 'X',
+        50: 'L',
+        100: 'C',
+        500: 'D',
+        1000: 'M'
+    }
+    const specialMap = {
+        4: 'IV',
+        9: 'IX',
+        40: 'XL',
+        90: 'XC',
+        400: 'CD',
+        900: 'CM'
+    }
+    const level = ['I', 'X', 'C', 'M']
+    for (let i = 0; i < arr.length; i++) {
+        const temp = parseInt(arr[i])
+        const left = 10 ** i
+        if (specialMap[temp * left] || nomallMap[temp * left]) {
+            res.unshift(specialMap[temp * left] || nomallMap[temp * left])
+        }
+        if (temp > 1 && temp < 4) {
+            let newTemp = temp
+            while (newTemp) {
+                res.unshift(level[i])
+                newTemp--
+            }
+        }
+        if (temp > 5 && temp < 9) {
+            let number = 'V'
+            let newTemp = temp
+            while (newTemp > 5) {
+                number += level[i]
+                newTemp--
+            }
+            res.unshift(number)
+        }
+    }
+    return res.join('')
+};
+console.log(intToRoman(1994))
