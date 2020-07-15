5c65ad0e73091e134952e3cc417af2da4b65fedc
xubin <xpin_1509@163.com>
2020-07-15 17:53:14 +0800

    组合

--- js/top.js
+++ js/top.js
@@ -1854,5 +1854,142 @@ var restoreIpAddresses = function(s) {
  * @return {number}
  */
 var numDecodings = function(s) {
+    // const res = []
+    // const obj = {}
+    // function combin (path, left) {
+    //     if (left == '' && !obj[path.join()]) {
+    //         res.push(path.join())
+    //         obj[path.join()] = 1
+    //         return
+    //     }
+    //     const len = Math.min(left.length, 2)
+    //     for (let i = 1; i <= 2; i++) {
+    //         const num = left.slice(0, i)
+    //         if (!isValid(num)) continue
+    //         path.push(num)
+    //         combin(path, left.slice(i))
+    //         path.pop()
+    //     }
+    // }
+    // function isValid (num) {
+    //     if (num.length > 1 && num.charAt(0) === '0') return false
+    //     return num >= 1 && num <= 26
+    // }
+    // combin([], s)
+    // return res.length
+    if (!s) return 1;
+    let cnt = 0
+    if (s[0] !== '0') cnt += numDecodings(s.slice(1));
+    if (s.length >= 2 && s[0] === '1' || (s[0] === '2' && s[1] >= 0 && s[1] <= 6)) {
+        cnt += numDecodings(s.slice(2));
+    }
+    return cnt;
 
+    // 作者：jsyt
+    // 链接：https://leetcode-cn.com/problems/decode-ways/solution/yi-kan-jiu-dong-san-chong-jie-fa-di-gui-dai-bei-wa/
+    // 来源：力扣（LeetCode）
+    // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+};
+
+// 75. 颜色分类
+// 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+// 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+// 注意:
+// 不能使用代码库中的排序函数来解决这道题。
+
+// 输入: [2,0,2,1,1,0]
+// 输出: [0,0,1,1,2,2]
+// 进阶：
+
+// 一个直观的解决方案是使用计数排序的两趟扫描算法。
+// 首先，迭代计算出0、1 和 2 元素的个数，然后按照0、1、2的排序，重写当前数组。
+// 你能想出一个仅使用常数空间的一趟扫描算法吗？
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] < nums[i]) {
+                let c = nums[i]
+                nums[i] = nums[j]
+                nums[j] = c
+            }
+        }
+    }
+    return nums
+};
+// 79. 单词搜索
+// 给定一个二维网格和一个单词，找出该单词是否存在于网格中。
+// 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+// board =
+// [
+//   ['A','B','C','E'],
+//   ['S','F','C','S'],
+//   ['A','D','E','E']
+// ]
+
+// 给定 word = "ABCCED", 返回 true
+// 给定 word = "SEE", 返回 true
+// 给定 word = "ABCB", 返回 false
+// 提示：
+
+// board 和 word 中只包含大写和小写英文字母。
+// 1 <= board.length <= 200
+// 1 <= board[i].length <= 200
+// 1 <= word.length <= 10^3
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+
+};
+// 77. 组合
+// 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+// 输入: n = 4, k = 2
+// 输出:
+// [
+//   [2,4],
+//   [3,4],
+//   [2,3],
+//   [1,2],
+//   [1,3],
+//   [1,4],
+// ]
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    const res = []
+    function combin (path, left) {
+        if (path.length === k) {
+            res.push([...path])
+            return
+        }
+        for (let i = 0; i < left.length; i++) {
+            const el = left[i]
+            if (!isValid(el, path[path.length - 1])) continue
+            path.push(el)
+            left.splice(i, 1)
+            combin(path, left)
+            path.pop()
+            left.splice(i, 0, el)
+        }
+    }
+    const left = []
+    for (let i = 1; i <= n; i++) {
+        left.push(i)
+    }
+    function isValid (num, standard = 0) {
+        return num > standard
+    }
+    combin([], left)
+    return res
 };
