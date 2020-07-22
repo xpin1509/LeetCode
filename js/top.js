59378462d2c3b559369a46278ad1b2a9a57a84b5
xubin <xpin_1509@163.com>
2020-07-22 10:49:01 +0800

    矩阵置零

--- js/top.js
+++ js/top.js
@@ -2192,3 +2192,67 @@ var simplifyPath = function(path) {
     }
     return '/' + res.join('/')
 };
+
+// 给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
+
+// 输入: 
+// [
+//   [1,1,1],
+//   [1,0,1],
+//   [1,1,1]
+// ]
+// 输出: 
+// [
+//   [1,0,1],
+//   [0,0,0],
+//   [1,0,1]
+// ]
+
+// 输入: 
+// [
+//   [0,1,2,0],
+//   [3,4,5,2],
+//   [1,3,1,5]
+// ]
+// 输出: 
+// [
+//   [0,0,0,0],
+//   [0,4,5,0],
+//   [0,3,1,0]
+// ]
+// 进阶:
+// 一个直接的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
+// 一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+// 你能想出一个常数空间的解决方案吗
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    const m = matrix.length
+    const n = matrix[0].length
+    const cols = []
+    const rows = []
+    for (let i = 0; i < m; i++) {
+        let rowHasZero = false
+        if (matrix[i].indexOf(0) > -1) {
+            rowHasZero = true
+            rows.push(i)
+        }
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                cols.push(j)
+            }
+            if (rowHasZero || cols.indexOf(j) > -1) {
+                matrix[i][j] = 0
+            }
+        }
+    }
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (cols.indexOf(j) > -1) {
+                matrix[i][j] = 0
+            }
+        }
+    }
+};
