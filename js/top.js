a06aed1ea0eea7609ceec2ef4d3b26f9a769f1d5
xubin <xpin_1509@163.com>
2020-07-22 11:12:31 +0800

    搜索二维矩阵

--- js/top.js
+++ js/top.js
@@ -2193,6 +2193,7 @@ var simplifyPath = function(path) {
     return '/' + res.join('/')
 };
 
+// 矩阵置零
 // 给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
 
 // 输入: 
@@ -2255,4 +2256,41 @@ var setZeroes = function(matrix) {
             }
         }
     }
+};
+
+// 74. 搜索二维矩阵
+// 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+
+// 每行中的整数从左到右按升序排列。
+// 每行的第一个整数大于前一行的最后一个整数。
+
+// 输入:
+// matrix = [
+//   [1,   3,  5,  7],
+//   [10, 11, 16, 20],
+//   [23, 30, 34, 50]
+// ]
+// target = 3
+// 输出: true
+
+// 输入:
+// matrix = [
+//   [1,   3,  5,  7],
+//   [10, 11, 16, 20],
+//   [23, 30, 34, 50]
+// ]
+// target = 13
+// 输出: false
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i].indexOf(target) > -1) {
+            return true
+        }
+    }
+    return false
 };
