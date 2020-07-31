7e2cf501fa6b330bea2a2d20f29aa5c7a5926774
xubin <xpin_1509@163.com>
2020-07-31 11:21:36 +0800

    换瓶子

--- js/jzoffer.js
+++ js/jzoffer.js
@@ -148,3 +148,40 @@ var numWays = function(n) {
     }
     return res[n]
 };
+
+// 1518. 换酒问题
+// 小区便利店正在促销，用 numExchange 个空酒瓶可以兑换一瓶新酒。你购入了 numBottles 瓶酒。
+
+// 如果喝掉了酒瓶中的酒，那么酒瓶就会变成空的。
+
+// 请你计算 最多 能喝到多少瓶酒。
+// 输入：numBottles = 9, numExchange = 3
+// 输出：13
+// 解释：你可以用 3 个空酒瓶兑换 1 瓶酒。
+// 所以最多能喝到 9 + 3 + 1 = 13 瓶酒。
+
+// 输入：numBottles = 15, numExchange = 4
+// 输出：19
+// 解释：你可以用 4 个空酒瓶兑换 1 瓶酒。
+// 所以最多能喝到 15 + 3 + 1 = 19 瓶酒。
+
+// 输入：numBottles = 5, numExchange = 5
+// 输出：6
+
+// 输入：numBottles = 2, numExchange = 3
+// 输出：2
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+var numWaterBottles = function(numBottles, numExchange) {
+    let total = numBottles
+    let empty = numBottles
+    while (empty >= numExchange) {
+        const newBottle = parseInt(empty / numExchange, 10)
+        total += newBottle
+        empty = newBottle + (empty % numExchange)
+    }
+    return total
+};
