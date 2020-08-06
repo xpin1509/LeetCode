17cb004f4e974643cdf732141e5a4b79bf7ccd79
xubin <xpin_1509@163.com>
2020-08-06 11:39:29 +0800

    旋转数组

--- js/code.js
+++ js/code.js
@@ -1340,3 +1340,38 @@ var nextPermutation = function(nums) {
         nums[j] = t;
     }
 }
+
+// 189. 旋转数组
+// 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+// 示例 1:
+
+// 输入: [1,2,3,4,5,6,7] 和 k = 3
+// 输出: [5,6,7,1,2,3,4]
+// 解释:
+// 向右旋转 1 步: [7,1,2,3,4,5,6]
+// 向右旋转 2 步: [6,7,1,2,3,4,5]
+// 向右旋转 3 步: [5,6,7,1,2,3,4]
+// 示例 2:
+
+// 输入: [-1,-100,3,99] 和 k = 2
+// 输出: [3,99,-1,-100]
+// 解释: 
+// 向右旋转 1 步: [99,-1,-100,3]
+// 向右旋转 2 步: [3,99,-1,-100]
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    const arr = []
+    for (let i = 0; i < nums.length; i++) {
+        let index = i + k
+        while (index >= nums.length) {
+            index = index - nums.length
+        }
+        arr[index] = nums[i]
+    }
+    arr.map((el, index) => nums[index] = arr[index])
+};
