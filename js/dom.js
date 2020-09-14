155c242a3cc3d049908e18a00bd49404c2d828a4
xubin <xpin_1509@163.com>
2020-09-14 11:03:09 +0800

    dom同步

--- js/dom.js
+++ js/dom.js
@@ -31,3 +31,44 @@ function clickHandle () {
     // }
     // $dom.appendChild(fragMent)
 }
+
+function syncDom () {
+    // 插入十万条数据
+    const total = 100000
+    // 一次插入 20 条，如果觉得性能不好就减少
+    let ul = document.querySelector('ul')
+    for (let i = 0; i < total; i++) {
+        const li = document.createElement('li')
+        li.innerText = Math.floor(Math.random() * total)
+        ul.appendChild(li)
+    }
+}
+// syncDom()
+//     setTimeout(() => {
+//         // 插入十万条数据
+//         const total = 100000
+//         // 一次插入 20 条，如果觉得性能不好就减少
+//         const once = 20
+//         // 渲染数据总共需要几次
+//         const loopCount = total / once
+//         let countOfRender = 0
+//         let ul = document.querySelector('ul')
+//         function add() {
+//             // 优化性能，插入不会造成回流
+//             const fragment = document.createDocumentFragment()
+//             for (let i = 0; i < once; i++) {
+//                 const li = document.createElement('li')
+//                 li.innerText = Math.floor(Math.random() * total)
+//                 fragment.appendChild(li)
+//             }
+//             ul.appendChild(fragment)
+//             countOfRender += 1
+//             loop()
+//         }
+//         function loop() {
+//             if (countOfRender < loopCount) {
+//                 window.requestAnimationFrame(add)
+//             }
+//         }
+//         loop()
+//   }, 0)
