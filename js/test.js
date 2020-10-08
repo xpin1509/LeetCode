d454ba6cff46d5e7f72049505c4e59cb62bd1ea2
xubin <xpin_1509@163.com>
2020-10-08 16:58:39 +0800

    图片懒加载

--- js/test.js
+++ js/test.js
@@ -1,3 +1,161 @@
+// function getTarget (arr, target) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i; j < arr.length; j++) {
+//             if ((arr[i] + arr[j]) === target) {
+//                 return [i, j]
+//             }
+//         }
+//     }
+// }
+// console.log(getTarget([2,7,13,15], 9))
+// function sortFn (arr, k) {
+//     const res = arr.sort((a,b) => {
+//         return b - a
+//     })
+//     return res[k-1]
+// }
+// console.log(sortFn([3,2,3,1,2,4,5,5,6], 4))
+// 现给定任意正整数 n，请寻找并输出最小的正整数 m（m>9），
+// 使得 m 的各位（个位、十位、百位 ... ...）之乘积等于n，若不存在则输出 -1。
+function solution( n ) {
+    // for (let i = 9; i <= 10 ** `${n}`.length; i++ ){
+    //     if (getSum(i) === n) {
+    //         return i
+    //     }
+    // } 
+    // function getSum (x) {
+    //     let res = 1
+    //     var arr = `${x}`.split('')
+    //     for(let i = 0; i < arr.length; i++ ) {
+    //         res *= arr[i]
+    //     }
+    //     return res
+    // }
+    // return -1
+    // 求所有积为n的数组
+    // for (let i = 1; i < n; i++) {
+    //     if (n % i)
+    // }
+    // function getAllChild (x) {
+    //     const arr = []
+    //     for (let i = 2; i < x; i++) {
+    //         if (x % i === 0) {
+    //             arr.push(i)
+    //         }
+    //     }
+    // }
+}
+
+// 1 块钱可以买 3 个桃子吃，吃完后 3 个桃核可以换 1 个桃子，请问 135142857 元可以最多吃到多少个桃子。
+function getpeach (m) {
+    let peach = m * 3
+    let peachCore = m * 3
+    while (peachCore > 2) {
+        const newpeach = parseInt(peachCore/3)
+        peach += newpeach
+        peachCore -= newpeach * 3
+        peachCore += newpeach
+    }
+    return peach
+}
+// getpeach(1) === 4
+// getpeach(2) === 3 + 3 + 2 === 8
+// getpeach(3) === 3 + 3 + 3 + 3 + 1 === 13
+// console.log(getpeach(135142857))
+
+/**
+ * 十亿元以内,两位以内小数的中文表示法 
+ * 1 => '一'
+ * 11 => '十一'
+ * 21 => '二十一'
+ * 111 => '一百一十一'
+ * 1111 => '一千一百一十一'
+ * 11111 => '一万一千一百一十一'
+ * 111111 => '十一万一千一百一十一'
+ * 1111111 => '一百一十一万一千一百一十一'
+ */
+function numToChar(target) {
+    if (typeof target !== 'number' || target > 10 ** 9 || target < 0.01) return ''
+    const wList = ['', '万', '亿']
+    const digitsList = chunk((target + '').split('').reverse(), 4)
+    let arrList = digitsList.map((el,index) => {
+        return wList[index] ? [...removeLastZero(underthoundnumToChar(el)), wList[index]] : underthoundnumToChar(el)
+    })
+    // 扁平化
+    arrList = arrList.reverse().reduce((total, cur) => { return [...total, ...cur]}, [])
+    return removeTen(removeLastZero(remoMidZero(arrList))).join('')
+    // 数组分组
+    function chunk (arr, target) {
+        if (!Array.isArray(arr) || !target) {
+            console.error('请输入数组和有效数值')
+            return false
+        }
+        if (arr.length < target) return [arr]
+        const res = []
+        const arrTemp = [...arr]
+        while(arrTemp.length > target) {
+            res.push(arrTemp.splice(0, target))
+        }
+        return arrTemp.length ? [...res, arrTemp] : [...res]
+    }
+    // 10000以内的中文表示
+    function underthoundnumToChar (arr) {
+        const numList = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九']
+        const tennumList = ['', '十', '百', '千']
+        const res = []
+        for (let i = 0; i < arr.length; i++) {
+            let num = numList[arr[i]]
+            let last = tennumList[i]
+            res.push(replaceStr(num + last))
+        }
+        return res.reverse()
+    }
+    // 零百零千的转零
+    function replaceStr(str) {
+        return str.indexOf('零') > -1 ? '零' : str
+    }
+    // 去尾零
+    function removeLastZero(arr) {
+        const temp = [...arr]
+        while (temp.length) {
+            const last = temp.pop()
+            if (last !== '零') {
+                return [...temp, last]
+            }
+        }
+    }
+    // 去中间零
+    function remoMidZero (arr) {
+        if (arr.length < 4) return arr
+        for (let i = 1; i < arr.length - 1; i++) {
+            if (arr[i] == '零' && arr[i+1] == '零') {
+                arr.splice(i, 1)
+                i--
+            }
+        }
+        return arr
+    }
+    // 没有前置时，一十转十
+    function removeTen(arr) {
+        const temp = [...arr]
+        const first = temp.shift()
+        return first == '一十' ? ['十', temp] : arr
+    }
+}
+{/* <script>
+    console.log(1)
+    new Promise((r) => r()).then(() => {
+        console.log('then')
+    })
+    console.log(2)
+</script>
+<script>
+    console.log('3')
+</script> */}
+// 1 2 then 3
+
+
+
 // 1.Promise.all
 Promise.all = function (list) {
     const obj = {}
