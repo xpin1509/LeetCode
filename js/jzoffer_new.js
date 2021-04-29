187b1692c932fa44d3bd5e3ce6104614985dd80d
xubin <xpin_1509@163.com>
2021-04-29 17:59:53 +0800

    jzoffer

--- js/jzoffer_new.js
+++ js/jzoffer_new.js
@@ -17,12 +17,186 @@
  * @return {number[]}
  */
 var reversePrint = function (head) {
-    if (!head) return [];
-    const result = [];
-    let cur = head;
-    while (cur) {
-        result.unshift(cur.val);
-        cur = cur.next;
+  if (!head) return [];
+  const result = [];
+  let cur = head;
+  while (cur) {
+    result.unshift(cur.val);
+    cur = cur.next;
+  }
+  return result;
+};
+
+// 剑指 Offer 07. 重建二叉树
+// 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+// 如，给出
+
+// 前序遍历 preorder = [3,9,20,15,7]
+// 中序遍历 inorder = [9,3,15,20,7]
+// 返回如下的二叉树：
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {};
+
+// 剑指 Offer 09. 用两个栈实现队列
+// 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+// 输入：
+// ["CQueue","appendTail","deleteHead","deleteHead"]
+// [[],[3],[],[]]
+// 输出：[null,null,3,-1]
+
+// 输入：
+// ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+// [[],[],[5],[2],[],[]]
+// 输出：[null,-1,null,null,5,2]
+
+var CQueue = function () {
+  this.list = [];
+};
+
+/**
+ * @param {number} value
+ * @return {void}
+ */
+CQueue.prototype.appendTail = function (value) {
+  this.list.push(value);
+};
+
+/**
+ * @return {number}
+ */
+CQueue.prototype.deleteHead = function () {
+  const result = this.list.shift();
+  return result || -1;
+};
+
+// 剑指 Offer 11. 旋转数组的最小数字
+// 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+// 输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
+// 例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+
+// 输入：[3,4,5,1,2]
+// 输出：1
+
+// 输入：[2,2,2,0,1]
+// 输出：0
+/**
+ * @param {number[]} numbers
+ * @return {number}
+ */
+var minArray = function(numbers) {
+    let len = numbers.length
+    let min = numbers[0]
+    for (let i = 1; i < len; i++) {
+        if (numbers[i] < min) {
+            min = numbers[i]
+        }
     }
-    return result;
+    return min
+};
+
+
+// 剑指 Offer 15. 二进制中1的个数
+// 请实现一个函数，输入一个整数（以二进制串形式），输出该数二进制表示中 1 的个数。
+// 例如，把 9 表示成二进制是 1001，有 2 位是 1。因此，如果输入 9，则该函数输出 2。
+
+// 输入：00000000000000000000000000001011
+// 输出：3
+// 解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。
+
+// 输入：00000000000000000000000010000000
+// 输出：1
+// 解释：输入的二进制串 00000000000000000000000010000000 中，共有一位为 '1'。
+
+// 输入：11111111111111111111111111111101
+// 输出：31
+// 解释：输入的二进制串 11111111111111111111111111111101 中，共有 31 位为 '1'。
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+ var hammingWeight = function(n) {
+    const str = n.toString(2)
+    let len = str.length
+    let result = 0
+    while(len >= 0) {
+        if (str.charAt(len) == 1) {
+            result += 1
+        }
+        len --
+    }
+    return result
+};
+
+// 剑指 Offer 17. 打印从1到最大的n位数
+// 输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+
+// 输入: n = 1
+// 输出: [1,2,3,4,5,6,7,8,9]
+
+// 说明：
+
+// 用返回一个整数列表来代替打印
+// n 为正整数
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var printNumbers = function (n) {
+  const end = 10 ** n - 1;
+  const result = [];
+  let i = 1;
+  while (i <= end) {
+    result.push(i);
+    i++
+  }
+  return result;
+};
+
+// 剑指 Offer 18. 删除链表的节点
+// 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+
+// 返回删除后的链表的头节点。
+
+// 输入: head = [4,5,1,9], val = 5
+// 输出: [4,1,9]
+// 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+
+// 输入: head = [4,5,1,9], val = 1
+// 输出: [4,5,9]
+// 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var deleteNode = function(head, val) {
+    if (head.val === val) return head.next
+    let pre = head, cur = head.next
+
+    while (cur && cur.val !== val) {
+        pre = cur
+        cur = cur.next
+    }
+
+    if (cur) pre.next = cur.next
+    return head
 };
