48822341f73ec71d05f87ddae9f1d3ea980230b6
xubin30 <xubin30@meituan.com>
2021-05-22 14:20:31 +0800

    更新

--- js/jzoffer_new.js
+++ js/jzoffer_new.js
@@ -820,3 +820,33 @@ var getLeastNumbers = function(arr, k) {
 
 
 // console.log(getLeastNumbers([0,0,1,2,4,2,2,3,1,4], 8))
+
+// 剑指 Offer 42. 连续子数组的最大和
+// 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+
+// 要求时间复杂度为O(n)。
+
+// 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// 输出: 6
+// 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    // 1.暴力法 O(n^2)
+    // if (!nums.length) return 0
+    // let result = nums[0]
+    // for (let i = 0; i < nums.length; i++) {
+    //     let tempRes = [nums[i]], total = nums[i]
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         total += nums[j]
+    //         tempRes.push(total)
+    //     }
+    //     const tempMax = Math.max(...tempRes)
+    //     if (tempMax > result) {
+    //         result = tempMax
+    //     }
+    // }
+    // return result
+};
