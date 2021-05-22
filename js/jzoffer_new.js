364b59d9993b96bfcaf11522c42fc360fce4e552
xubin30 <xubin30@meituan.com>
2021-05-22 15:30:55 +0800

    fix: 第一个只出现一次的字符

--- js/jzoffer_new.js
+++ js/jzoffer_new.js
@@ -858,3 +858,37 @@ var maxSubArray = function(nums) {
     }
     return res
 };
+
+
+// 剑指 Offer 50. 第一个只出现一次的字符
+// 在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+
+// s = "abaccdeff"
+// 返回 "b"
+
+// s = "" 
+// 返回 " "
+
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var firstUniqChar = function(s) {
+    const map = {}
+    for (let i of s) {
+        if (map[i]) {
+            map[i] += 1
+        } else {
+            map[i] = 1
+        }
+    }
+    let result = ' '
+    for (let i of Object.entries(map)) {
+        const [key, value] = i
+        if (value === 1) {
+            result = key
+            break
+        } 
+    }
+    return result
+};
