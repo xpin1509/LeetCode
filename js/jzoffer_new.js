9d5f7dfe918f58d24d0989640f93d3121c6e680e
xubin30 <xubin30@meituan.com>
2021-05-25 17:28:41 +0800

    fix: 在排序数组中查找数字 I

--- js/jzoffer_new.js
+++ js/jzoffer_new.js
@@ -885,3 +885,30 @@ var firstUniqChar = function(s) {
     }
     return result
 };
+
+// 剑指 Offer 53 - I. 在排序数组中查找数字 I
+// 统计一个数字在排序数组中出现的次数。
+
+// 输入: nums = [5,7,7,8,8,10], target = 8
+// 输出: 2
+
+// 输入: nums = [5,7,7,8,8,10], target = 6
+// 输出: 0
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    const map = new Map();
+    for (num of nums) {
+        if (map.has(num)) {
+            const times = map.get(num)
+            map.set(num, times + 1)
+        } else {
+            map.set(num, 1)
+        }
+    }
+    if (map.has(target)) return map.get(target)
+    return 0
+};
