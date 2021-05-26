8fd1dc56dda8aa3834a36039e5f2a23b1b5d1b74
xubin30 <xubin30@meituan.com>
2021-05-26 10:54:02 +0800

    fix: 翻转单词顺序

--- js/jzoffer_new.js
+++ js/jzoffer_new.js
@@ -1090,25 +1090,64 @@ var twoSum = function(nums, target) {
  * @return {number[][]}
  */
 var findContinuousSequence = function(target) {
-    const result = []
-    function combin (current, left) {
-        const total = current.reduce((total, cur) => total + cur, 0);
-        if (total === target) {
-            return result.push(current)
-        }
-        combin([...current, cur], left.filter(el => el > cur))
-    }
-    combin([], getLeftNum(1, 9))
+    // const result = []
+    // function combin (current, left) {
+    //     const total = current.reduce((total, cur) => total + cur, 0);
+    //     if (total === target) {
+    //         return result.push(current)
+    //     }
+    //     combin([...current, cur], left.filter(el => el > cur))
+    // }
+    // combin([], getLeftNum(1, 9))
 
     function getLeftNum (start, end) {
         const result = []
-        for (let i = start; i < end; i++) {
+        for (let i = start; i <= end; i++) {
             result.push(i)
         }
         return result
     }
 
-    return result
+    const resultArr = []
+    for (let i = 1; i < target; i++) {
+        let result = i
+        for (let j = i + 1; j < target; j++) {
+            result += j
+            if (result === target) {
+                resultArr.push(getLeftNum(i, j))
+            } else if (result > target) {
+                break
+            }
+        }
+    }
+    return resultArr
 };
 
-// console.log(findContinuousSequence(9))
+// 剑指 Offer 58 - I. 翻转单词顺序
+// 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。
+// 为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+
+// 输入: "the sky is blue"
+// 输出: "blue is sky the"
+
+// 输入: "  hello world!  "
+// 输出: "world! hello"
+// 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+
+// 输入: "a good   example"
+// 输出: "example good a"
+// 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+//  
+
+// 无空格字符构成一个单词。
+// 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+// 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    s = s.replace(/\s+/g, ' ')
+    const alphaArr = s.split(' ').filter(el => el !== '');
+    return alphaArr.reverse().join(' ')
+};
