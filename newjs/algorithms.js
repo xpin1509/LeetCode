e03e160f6ba793288b2afae7cf6114fe85df5bf5
xubin30 <xubin30@meituan.com>
2021-11-14 15:24:21 +0800

    feat: add same tree

--- newjs/algorithms.js
+++ newjs/algorithms.js
@@ -327,6 +327,19 @@ function minDepth (node) {
     return min + 1
 }
 
+// 100. 相同的树
+// 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+// 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+var isSameTree = function(p, q) {
+    if (!p && !q) return true
+
+    if (!p || !q) return false
+
+    if (p.val !== q.val) return false
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
+
 // 二叉树题：二叉树遍历的姿势 深度优先
 // 先序，中序，后序 ｜ 广度优先
 function preorder(root) {
