d040e98423b76f1eaf3a2d2608c3a84478174486
xubin30 <xubin30@meituan.com>
2021-11-28 22:15:29 +0800

    fix：delete

--- newjs/errorHandle.js
+++ newjs/errorHandle.js
@@ -49,3 +49,22 @@ async function parent_child() {
 // console.log(EL2)
 
 // 立刻执行函数
+
+
+// 测试PostMessage的输出
+// console.log(1111)
+// window.postMessage('hello words1')
+// setTimeout(() => console.log('ste1'), 0)
+// window.addEventListener('message', function (e) {
+//     console.log(e)
+// }, false)
+// window.postMessage('hello words2')
+// setTimeout(() => console.log('ste1'), 0)
+// console.log(12121)
+// -----输出-----
+// 1111
+// errorHandle.js:61 12121
+// errorHandle.js:57 MessageEvent {isTrusted: true, data: 'hello words1', origin: 'null', lastEventId: '', source: Window, …}
+// errorHandle.js:57 MessageEvent {isTrusted: true, data: 'hello words2', origin: 'null', lastEventId: '', source: Window, …}
+// errorHandle.js:55 ste1
+// errorHandle.js:60 ste1
