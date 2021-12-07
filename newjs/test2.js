701131ce5e71b252367d794f736f5aefba182ff9
xubin30 <xubin30@meituan.com>
2021-12-07 14:52:36 +0800

    fix：add

--- newjs/test2.js
+++ newjs/test2.js
@@ -23,7 +23,6 @@ const rootTreeTempalte = {
       }
     }
   };
-
 // 递归
 function traverse () {
     const result = []
@@ -441,22 +440,388 @@ var inorderTraversal3 = function(root) {
     return result.reverse
 };
 
-// 栈和队列：// 如何用栈实现一个对列 最小栈，
+
+// 二叉树的前中后序遍历，迭代
+// rootTreeTempalte
+// 前序遍历
+function prologueTraversal (root) {
+    const stack = [];
+    const result = []
+    stack.push(root)
+    while (stack.length) {
+
+        root = stack.pop()
+        if (!root) continue
+
+        result.push(root.val)
+
+        stack.push(root.right)
+
+        stack.push(root.left)
+    }
+    return result
+}
+// 后序遍历
+function postSequenceTraversal (root) {
+    const stack = []
+    const result = []
+    stack.push(root)
+    while (stack.length) {
+        root = stack.pop()
+        if (!root) continue
+
+        result.push(root.val)
+        stack.push(root.left)
+        stack.push(root.right)
+    }
+    return result.reverse()
+}
+function middleOrderTraversal (root) {
+    const stack = []
+    const result = []
+    while (stack.length || root) {
+        while (root) {
+            stack.push(root)
+            root = root.left
+        }
+
+        root = stack.pop()
+        if (!root) continue
+
+        result.push(root.val)
+        root = root.right
+    }
+    return result
+}
+
+
+// 11. 盛最多水的容器
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+ var maxArea = function(height) {
+    let L = 0, R = height.length - 1
+    let Max = 0
+    while (L < R) {
+        const high = Math.min(height[L], height[R])
+        Max = Math.max(Max, high * (R - L))
+        if (height[L] > height[R]) {
+            R--
+        } else if (height[L] < height[R]) {
+            L++
+        } else {
+            L++
+            R--
+        }
+    }
+    return Max
+ }
+
+ // 53. 最大子数组和 
+var maxSubArray = function(nums) {
+    const result = [nums[0]]
+    for (let i = 1; i < nums.length; i++) {
+        result[i] = Math.max(result[i - 1] + nums[i], nums[i])
+    }
+    return Math.max(...result)
+}
+
+// 剑指 Offer II 119. 最长连续序列
+// 可以设计并实现时间复杂度为 O(n) 的解决方案吗？
+var longestConsecutive = function(nums) {
+    if (!nums.length) return []
+    nums.sort((a, b) => a - b)
+    let length = 1
+    let max = 1
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) continue
+        if (nums[i] === nums[i-1] + 1) {
+            length ++
+        } else {
+            max = Math.max(length, max) 
+            length = 1
+        }
+    }
+    return Math.max(max, length) 
+};
+// 674. 最长连续递增序列
+var findLengthOfLCIS = function(nums) {
+    if (nums.length === 1) return 1
+    let lastMax = 1
+    let length = 1
+    for (let i = 1; i < nums.length - 1; i++) {
+        if (nums[i] > nums[i - 1]) {
+            length++
+        } else {
+            lastMax = Math.max(length, lastMax)
+            length = 1
+        }
+    }
+    return Math.max(lastMax, length)
+};
+// 1800. 最大升序子数组和
+var maxAscendingSum = function(nums) {
+    let maxAscendingSum = nums[0]
+    let curSum = nums[0]
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            curSum = nums[i] + curSum
+        } else {
+            maxAscendingSum = Math.max(maxAscendingSum, curSum)
+            curSum = nums[i]
+        }
+    }
+    return Math.max(maxAscendingSum, curSum)
+};
+// 3.三数求和问题
+// 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+// 给定数组 nums = [-1, 0, 1, 2, -1, -4]， 满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    if (nums.length < 3) return [];
+    nums = nums.sort((a, b) => a - b)
+    const result = []
+    for (var i = 0; i < nums.length - 2; i++) {
+        if (nums[0] > 0) break;
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue
+        }
+        let L = i + 1, R = nums.length - 1
+        while (L < R ) {
+            const sum = nums[i] + nums[L] + nums[R]
+            if (sum === 0) {
+                result.push([nums[i], nums[L], nums[R]])
+                while (L + 1 < R && nums[L+ 1] === nums[L]) { L ++ }
+                while (L < R - 1 && nums[R - 1] === nums[R]) { R -- }
+                L++; R--
+            } else if (sum < 0) {
+                while (L + 1 < R && nums[L+ 1] === nums[L]) { L ++ }
+                L++
+            } else if (sum > 0) {
+                while (L < R - 1 && nums[R - 1] === nums[R]) { R -- }
+                R--
+            }
+        }
+    }
+    return result
+}
+// 108. 将有序数组转换为二叉搜索树 TODO
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+ var sortedArrayToBST = function(nums) {
+    function helper (nums, L, R) {
+        if (R > L) return null
+        const mid = Math.floor((L + R) / 2)
+        const root = new TreeNode(nums[mid])
+
+        root.left = helper(nums, L, mid - 1)
+        root.right = helper(nums, mid + 1, R)
+        return root
+    }
+    return helper(nums, 0, nums.length - 1)
+}
+// 平衡二叉树
+var isBalanced = function(root) {
+    if (!root) return true
+
+    return Math.abs(maxDepth(root.left) - maxDepth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+
+    function maxDepth (root) {
+        if (!root) return 0
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+    }
+};
+// 路径总和
+var hasPathSum = function(root, targetSum) {
+    if (!root) return false
+
+    if (!root.left && !root.right) return root.val === targetSum
+
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
+};
+// 对称二叉树
+var isSymmetric = function(root) {
+    if (!root) return true
+    function helper (left, right) {
+        if (!left && !right) return true
+        if (!left || !right) return false
+
+        return left.val === right.val && helper(left.left, right.right) && helper(left.right, right.left)
+    }
+
+    return helper(root.left, root.right)
+}
+
+// 最小硬币找零
+function minCoinChange (coins, amount) {
+    const makeChange = (value) => {
+        let min = []
+        if (value < 0) return min
+        for (let i = 0; i < coins.length; i++) {
+            const coin = coins[i]
+            if (value - coin >= 0) {
+                const newMin = makeChange(value - coin)
+                if (!min.length || newMin.length < min.length) {
+                    min = newMin.concat(coin)
+                }
+            }
+            
+        }
+        return min
+    }
+
+    return makeChange(amount)
+}
+// 反转链表
+var reverseList = function(head) {
+    let cur = head
+    let pre = null
+    while (cur) {
+        const temp = cur.next
+        cur.next = pre
+        
+        pre = cur
+        cur = temp
+    }
+    return pre
+};
+
+/**
+ * 
+ * TODO 下面的再做几遍
+ * @returns 
+ */
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+// 55. 跳跃游戏 TODO
+var canJump = function(nums) {
+    let reach = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (i > reach) {
+            return false
+        }
+        reach = Math.max(reach, nums[i] + i)
+    }
+    return true
+};
+
+// 45. 跳跃游戏 II TODO
+
+// 合并两个有序链表 TODO
+var mergeTwoLists = function(list1, list2) {
+    const pre = new ListNode()
+    let cur = pre
+
+    while (list1 && list2) {
+        if (list1.val > list2.val) {
+            cur.next = list2
+            list2 = list2.next
+        } else {
+            cur.next = list1
+            list1 = list1.next
+        }
+        cur = cur.next
+    }
+
+    cur.next = list1 || list2
+    
+    return pre.next
+};
+// const l1 = new ListNode(1)
+// l1.next = new ListNode(2)
+// l1.next.next = new ListNode(3)
+// l1.next.next.next = new ListNode(14)
+// l1.next.next.next.next = null
+
+// const l2 = new ListNode(1)
+// l2.next = new ListNode(3)
+// l2.next.next = new ListNode(4)
+// console.log(mergeTwoLists(l1, l2))
+
+
+// 128. 最长连续序列 TODO
+// 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+var longestConsecutive = function(nums) {
+    const set = new Set()
+    for (num in nums) {
+        set.add(num)
+    }
+    let length = 0
+    
+    for (let item of set) {
+        if (!set.has(item - 1)) {
+            let currentNum = item
+            let currentStreak = 1;
+
+            while (set.has(currentNum + 1)) {
+                currentStreak++
+                currentNum ++
+            }
+            length = Math.max(length, currentStreak)
+        }
+    }
+    return length
+};
+// 83. 删除排序链表中的重复元素 TODO 一些次数
+// 输入: 1->1->2
+// 输出: 1->2
+// 示例 2:
+// 输入: 1->1->2->3->3
+// 输出: 1->2->3
+var deleteDuplicates = function(head) {
+    if (!head) return head
+
+    let cur = head
+    while (cur.next) {
+        if (cur.val === cur.next.val) {
+            cur.next = cur.next.next
+        } else {
+            cur = cur.next
+        }
+    }
+    return head
+};
+
+// 1 3 6 4 2
+// 下一个排列 TODO 一些次数
+var nextPermutation1 = function(nums) {
+    if (nums.length < 2) return nums
+    let i = nums.length - 2
+    while (i>= 0 && nums[i] >= nums[i+1]) {
+        i--
+    }
+    if (i >= 0) {
+        let j = nums.length - 1
+        while (j >= 0 && nums[i] >= nums[j]) {
+            j --
+        }
+        [nums[i], nums[j]] = [nums[j], nums[i]]
+    }
+    function reverse (nums, l, r) {
+        while (l < r) {
+            [nums[l], nums[r]] = [nums[r], nums[l]]
+            l++
+            r--
+        }
+    }
+    reverse(nums, i + 1, nums.length - 1)
+    return nums
+};
+
+// 有时间就写一下吧
+// 买卖股票简单题
+// “接雨水”问题 // https://juejin.cn/book/6844733800300150797/section/6844733800375648269
 // 链表题：反转链表，合并两个有序链表，链表节点的删除重复，删除倒数第N个节点，// 局部反转链表，判断环形链表，返回环的起点
-// 二叉树：最小深度，二叉搜索树，DFS（迭代法 // BFS，层序遍历 二叉搜索树，平衡二叉树的判断 最大深度 反转二叉树，
-// 动态规划：最少硬币数
-// 数组：三数之和
-// 最长上升子序列模型 ...
-// 滑动窗口：滑动窗口问题
 // promise
 // 并发请求限制
-// 千分位
 // compose
-// test1的DONE 和algorithms所有
-
-// “接雨水”问题 // https://juejin.cn/book/6844733800300150797/section/6844733800375648269
-
-// 真题描述：给你一个包含 n 个整数的数组 nums，
-// 判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？
-// 请你找出所有满足条件且不重复的三元组。
-
